6d6a9c9c647d588e78f4a930d7a09371815e9c9d
Rahul Manna <rahulmanna080@gmail.com>
2022-08-09 20:24:55 +0530

    Day_9 program added(Recursions)

---
+++ Day_9.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Day_9
+{
+    public static int factorial(int n){
+        if(n<=0)
+        {
+            return 1;
+        }
+        else {
+            return n * factorial(n-1);
+        }
+    }
+ public static void main(String[] args) {
+     Scanner sc=new Scanner(System.in);
+     int n= sc.nextInt();
+     System.out.println(factorial(n));
+
+}
+}
